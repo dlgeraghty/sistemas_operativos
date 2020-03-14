84ff13ba8161cb70de5fd2429e08de2fce5da507
dlgeraghty <dvd.loge@gmail.com>
2020-03-15 00:08:22 +0100

    the race condition is now solved but this is extremely slow!

--- threads4.c
+++ threads4.c
@@ -2,12 +2,15 @@
 #include <stdint.h>
 #include <pthread.h>
 
-#define BIG 1000000000UL
+#define BIG 1000000000
 uint32_t counter = 0;
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 
 void * count_to_big(void * arg){
 	for(uint32_t i = 0; i < BIG; i++){
+		pthread_mutex_lock(&lock);
 		counter++;
+		pthread_mutex_unlock(&lock);
 	}
 	return NULL;
 }
