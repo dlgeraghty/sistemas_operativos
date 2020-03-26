887eb34775b5a1920086e912f2d49f260314b9c0
dlgeraghty <dvd.loge@gmail.com>
2020-03-26 17:17:43 +0100

    added sigchld

--- ejercicios_procesos_codigo/11.c
+++ ejercicios_procesos_codigo/11.c
@@ -3,28 +3,40 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <signal.h>
+#include <fcntl.h>
 
-//lanzar a ejecucion dos programas simultaneamente manteniendo el padre vivo:
+static int n_child = 0;
+
+void handler(int sig){
+	pid_t pid;
+	pid = wait(NULL);
+	printf("Pid %d exited\n", pid);
+	n_child--;
+	printf("n_child [%d]\n", n_child);
+	if(n_child == 0){
+		printf("hijos, estoy orgulloso de vosotros :) ya puedo descansar en paz\n");
+		exit(0);
+	}
+}
 
 int main(int argc, char * argv[]){
 
+	signal(SIGCHLD,handler);
 	printf("Ola :P\n");
 	int i, status;
-	const char *programas[] = { "brave", "xournalpp"};
+	const char *programas[] = { "arandr", "xournalpp"};
 	int N = sizeof(programas) / sizeof(programas[0]);
 	pid_t pids[N];
+	n_child = N;
 	for(i = 0; i < N; i++){
 		pid_t pid = fork();
 		pids[i] = pid;
-		if(pid == 0)
+		if(pid == 0){
 			execlp(programas[i], programas[i], NULL);
+		}
 	}
-
-	for(int i = 0; i < N; i++){
-		pid_t cpid = waitpid(pids[i], &status, 0);
-		printf("the %s process finished\n", programas[i]);
-	}
-
+	getchar();
 	printf("adios :)\n");
 	return 0;
 }
