5b601507bf06a25fbf8151255dabfff6db9219bf
dlgeraghty <dvd.loge@gmail.com>
2020-04-26 23:23:07 +0200

    achieved thread syncrhonization

--- concurrencia/suma_paralela.c
+++ concurrencia/suma_paralela.c
@@ -1,32 +1,45 @@
-#include <stdlib.c>
-#include <stdio.c>
+#include <stdlib.h>
+#include <stdio.h>
 #include <pthread.h>
 #include <unistd.h>
 #include "barrera.h"
 
-#define NELEMS 16
-#define NTRHEADS 4
 
+int NELEMS, NTHREADS;
 barrier_t mybarrier;
-int elementos[NELEMS];
+int * elementos;
 
 void * parallel_sum(void * data){
 
-	int pos = 
-
+	int pos = *((int *) data);
+	printf("voy a poner el resultado de mi suma en la posicion: %d del vector\n", pos);
+	barrier_wait(&mybarrier);
 
 }
 
 int main(){
 
-	pthread_t ids[NTRHEADS];
+	NELEMS = 16;
+	NTHREADS = 4;
 
-	barrier_init(&mybarrier, NULL, NTHREADS);
+	elementos = (int *)malloc(NELEMS * sizeof(int));
 
-	for(int i = 0; i < NTHREADS; i++){
-		pthread_create(&ids[i], NULL, parallel_sum, i);
-	}	
+	pthread_t ids[NTHREADS];
+	int short_ids[NTHREADS];
 
 
-	
+	while(NTHREADS > 1){
+		barrier_init(&mybarrier, NULL, NTHREADS + 1);
+		printf("Numero de hilos a utilizar: %d\n", NTHREADS);
+		for(int i = 0; i < NTHREADS; i++){
+			short_ids[i] = i;
+			pthread_create(&ids[i], NULL, parallel_sum, &short_ids[i]);
+		}
+		barrier_wait(&mybarrier);
+
+		NTHREADS/=2;
+	}
+
+
+
 }
