80af25f3f1cf81a6371c1ab63a0eb6e9a5b84db2
dlgeraghty <dvd.loge@gmail.com>
2020-05-11 22:47:38 +0200

    prod/cons w/ sem_open (in progress)

--- concurrencia/shm.c
+++ concurrencia/shm.c
@@ -7,42 +7,70 @@
 #include <sys/wait.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
+#include <semaphore.h>
 
 //shared memory example very similar to: https://www.geeksforgeeks.org/posix-shared-memory-api/
 //but it uses fork and functions in the same file to make it happen rather than a file per use case.
+//Now it also implements the real producer consumer problem
 
 const int SIZE		=	4096;
 const char * name	=	"OS";
-const char * message1	=	"Hello";
-const char * message2	=	"World";
+const char * full_name	=	"full";
+const char * empty_name	=	"empty";
 int shm_fd;
 
+//semaphores:
+sem_t *full, *empty;
+
 void producer(void){
+
+	printf("Inicializando productor\n");
 
 	void * ptr;
 	ptr = mmap(0, SIZE, PROT_WRITE, MAP_SHARED, shm_fd, 0);
 
-	sprintf(ptr, "%s", message1); 
-	ptr += strlen(message1);
-	sprintf(ptr, "%s", message2);
-	ptr += strlen(message2);
+
+	sem_wait(full);
+	//generate random number:
+	int r = random();
+	char * rs;
+	sprintf(rs, "%d", r);
+	strcat(rs, "random number generated:");
+
+	sprintf(ptr, "%s", rs); 
+	ptr += strlen(rs);
+	sem_post(empty);
 	
 }
 
 void consumer(void){
 
+	printf("Inicializando consumidor\n");
+
+	sem_wait(empty);
 	void * ptr;
 
 	ptr = mmap(0, SIZE, PROT_READ, MAP_SHARED, shm_fd, 0);
 
 	printf("%s\n", (char*)ptr);
-
-	shm_unlink(name);
+	sem_post(full);
 
 
 }
 
 int main(){
+
+	full = sem_open(full_name, O_CREAT, 10);
+	empty = sem_open(empty_name, O_CREAT, 0);
+	
+	if(full == SEM_FAILED){
+		perror("sem_open(full)");
+		exit(-1);
+	}
+	if(empty == SEM_FAILED){
+		perror("sem_open(empty)");
+		exit(-1);
+	}
 
 	shm_fd = shm_open(name, O_CREAT | O_RDWR, 0666);
 
@@ -55,6 +83,10 @@ int main(){
 		consumer();
 
 	while(wait(NULL) > 0);
+
+	sem_destroy(empty);
+	sem_destroy(full);
+	shm_unlink(name);
 
 	return 0;
 	
