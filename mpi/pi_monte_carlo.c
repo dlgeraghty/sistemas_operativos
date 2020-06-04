3dbd7e8404be81149e63b47411185da4150db68a
dlgeraghty <dvd.loge@gmail.com>
2020-06-04 23:30:35 +0200

    first part of exercise

--- mpi/pi_monte_carlo.c
+++ mpi/pi_monte_carlo.c
@@ -1,36 +1,46 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include <mpi.h>
 
 //approximating PI value with Monte Carlo's algorithm 
 //source: https://computing.llnl.gov/tutorials/mpi/
 #define points 10000
-#define tasks 100
 
-int main(){
+int inside_circle(int x, int y){
+	return 0;
+}
 
-	//pseudocode:
-	//
-	//srand(null)
-	//
-	//num = points/tasks
-	//int circle_count = 0;
-	//
-	//generate processes:
-	//
-	//if WORKER:
-	//from(int i = 1; i < num; i++){
-	//
-	//	int x = random(0,1)
-	//	int y = random(0,1)
-	//
-	//	if(inside_circle(x,y))	inside_circle??
-	//		circle_count++;
-	//
-	//	send circle_count to master
-	//		
-	//if MASTER:
-	//	receive RESPONSE from workers		
-	//	calculate pi approx
-	//}
+int main(int *argc, char ** argv){
 
+	int num, circle_count, ierr, root_process, num_procs, my_id, i, x, y; 
+
+	srand(time(NULL));
+	circle_count = 0;
+
+	ierr = MPI_Init(&argc, &argv);
+
+	root_process = 0;
+
+	//get the id of the process
+	ierr = MPI_Comm_rank(MPI_COMM_WORLD, &my_id);
+	ierr = MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
+
+	num = points/num_procs;
+
+	if(my_id != root_process)
+		for(i = 1; i < num; i++){
+
+			x = random();
+			y = random();
+
+			if(inside_circle(x,y))
+				circle_count++;
+			//send circle_count to master;
+		}
+	else{
+
+		//receive circle_count
+	
+	}
 }
